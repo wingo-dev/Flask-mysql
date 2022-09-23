dba0743f8f378e9bea40a349b5ab99d9c389dfb0
Rismi Rukshan <rukshanlancer@gmail.com>
2022-09-23 09:16:20 -0600

    profile, home page nave menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
- 
+import MySQLdb.cursors
+
 app = Flask(__name__, template_folder='template')
  
 app.config['MYSQL_HOST'] = 'localhost'
@@ -12,7 +13,9 @@ mysql = MySQL(app)
 @app.route('/')
 def main():
     return render_template('form.html')
-
+@app.route('/home')
+def home():
+    return render_template('home.html')
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
     if request.method == 'GET':
@@ -26,5 +29,13 @@ def login():
         mysql.connection.commit()
         cursor.close()
         return render_template('home.html')
+@app.route('/profile')
+def profile():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM accounts WHERE id = 1')
+    account = cursor.fetchone()
+    # Show the profile page with account info
+    return render_template('profile.html', account=account)
+
 if __name__ == '__main__':
    app.run(debug = False)
